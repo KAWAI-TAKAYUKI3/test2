b3216a13a5d8de95df9bc9641c7d0f8954cd0b5c
KAWAI-TAKAYUKI3 <TAKAYUKI.KAWAI@mail.toyota-body.co.jp>
2025-12-09 20:59:11 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
 
+
 // 現在位置を取得の関数
 function success(position) {
   latitude = position.coords.latitude;
@@ -32,11 +33,17 @@ function makeTime() {
 let startTime = 0;
 let arrivalTime = 0;
 let startTriger = 0;
+let firstTriger = 0;
+let secondTriger = 0;
 let latitude = 0;
 let longitude = 0;
-let gpsAccuracy = 250; //単位はメートル
+let gpsAccuracy = 200; //単位はメートル
 let destinationLatitude = 0;
 let destinationLongitude = 0;
+let firstLatitude = 0;
+let firstLongitude = 0;
+let secondLatitude = 0;
+let secondLongitude = 0;
 let interval = 1;
 
 // 開始処理
@@ -51,14 +58,24 @@ function getStartTime() {
   //   document.getElementById("not-home").textContent = "自宅以外からは計測開始できません";
   //   return;
   // }
-  document.getElementById("start-time").textContent = makeTime();
-  document.getElementById("not-start").textContent =""
-  startTime = new Date().getTime();
-  startTriger = 1;
-  navigator.geolocation.watchPosition(success, error);
-  interval = setInterval(underMoving, 1000);
-  destinationLatitude = Number(document.getElementById("dest-latitude").value)
-  destinationLongitude = Number(document.getElementById("dest-longitude").value)
+  if (startTriger === 0) {
+    document.getElementById("start-time").textContent = makeTime();
+    document.getElementById("not-start").textContent =""
+    startTime = new Date().getTime();
+    startTriger = 1;
+    navigator.geolocation.watchPosition(success, error);
+    underMoving();
+    interval = setInterval(underMoving, 1000);
+    destinationLatitude = Number(document.getElementById("third-latitude").value)
+    destinationLongitude = Number(document.getElementById("third-longitude").value)
+    firstLatitude = Number(document.getElementById("first-latitude").value)
+    firstLongitude = Number(document.getElementById("first-longitude").value)
+    secondLatitude = Number(document.getElementById("second-latitude").value)
+    secondLongitude = Number(document.getElementById("second-longitude").value)
+    document.getElementById("arrival-time").textContent = "";
+    document.getElementById("spended-time").textContent = "";
+    document.getElementById("arrived").textContent = "";
+  }
 }
       
 // 測定関数
@@ -66,7 +83,12 @@ function underMoving() {
   let accuracy = gpsAccuracy * 0.00001;
   document.getElementById("current-latitude").textContent = latitude.toFixed(5);
   document.getElementById("current-longitude").textContent = longitude.toFixed(5);
-  console.log("test")
+  let startText = document.getElementById("start-bottun").textContent
+  if (startText === "出発！" || startText ==="実行中") {
+    document.getElementById("start-bottun").textContent = "実 行 中";
+  } else {
+    document.getElementById("start-bottun").textContent = "実行中";
+  }
   if (latitude > destinationLatitude - accuracy 
     && latitude < destinationLatitude + accuracy 
     && longitude > destinationLongitude - accuracy 
@@ -76,8 +98,29 @@ function underMoving() {
         document.getElementById("spended-time").textContent = getSpendTime(startTime, arrivalTime);
         document.getElementById("arrived").textContent = "到着しました🐢";
         clearInterval(interval);
+        document.getElementById("start-bottun").textContent = "到着済";
+  } else if (latitude > firstLatitude - accuracy 
+    && latitude < firstLatitude + accuracy 
+    && longitude > firstLongitude - accuracy 
+    && longitude < firstLongitudee + accuracy
+    && firstTriger === 0) {
+        document.getElementById("lap1").textContent = makeTime();
+        arrivalTime = new Date().getTime();
+        document.getElementById("lap1-spend").textContent = getSpendTime(startTime, arrivalTime);
+        firstTriger = 1;
+  } else if (latitude > secondLatitude - accuracy 
+    && latitude < secondLatitude + accuracy 
+    && longitude > secondLongitude - accuracy 
+    && longitude < secondLongitude + accuracy
+    && secondTriger === 0) {
+        document.getElementById("lap1").textContent = makeTime();
+        arrivalTime = new Date().getTime();
+        document.getElementById("lap1-spend").textContent = getSpendTime(startTime, arrivalTime);
+        secondTriger = 1;
   }
+  
 }
+
       
 // 所要時間計算関数
 function getSpendTime(start, arrival) {
@@ -89,6 +132,7 @@ function getSpendTime(start, arrival) {
   }
   return `${spendMinutes} : ${spendSeconds}`;
 }
+
 // テスト用到着処理
 function getArrivalTimeForTest() {
   if (startTriger !== 1) {
@@ -101,6 +145,8 @@ function getArrivalTimeForTest() {
   console.log(latitude, longitude, destinationLatitude, destinationLongitude);
   document.getElementById("arrived").textContent = "到着しました🐢";
   clearInterval(interval);
+  document.getElementById("start-bottun").textContent = "出発！"
+  startTriger = 0;
 }
 
 // テスト用位置捏造処理
